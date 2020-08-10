ee2d778cb68acaecdb00789f5c3617bf976185f9
Ankita <ankitapareek2000@gmail.com>
2020-08-10 15:43:04 +0530

    feat: Added NavBar and Alert dialog; conf: Created the project directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:hale_mate/screens/appointment.dart';
+import 'package:hale_mate/screens/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(HaleMateApp());
 }
 
-class MyApp extends StatelessWidget {
+class HaleMateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('HaleMate'),
+          appBar: AppBar(
+            title: Text('HaleMate'),
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+          body: DefaultTabController(
+              length: 5,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    constraints: BoxConstraints(maxHeight: 70.0),
+                    child: Material(
+                     color: Colors.deepPurpleAccent[400],
+                      child: TabBar(
+                        tabs: [
+                          Tab(icon: Icon(Icons.home), text: "Home"),
+                          Tab(icon: Icon(Icons.local_hospital), text: 'appointment'),
+                          Tab(icon: Icon(Icons.alarm), text: 'Alarm'),
+                          Tab(icon: Icon(Icons.account_circle), text: 'Profile',),
+                          Tab(icon: Icon(Icons.info), text: 'FAQ'),
+                        ],
+                        indicatorColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: TabBarView(
+                        children: [
+                          HomeScreen(),
+                          Icon(Icons.local_hospital),
+                          Icon(Icons.alarm),
+                          Icon(Icons.account_circle),
+                          Icon(Icons.info),
+                        ],  ))
+                ],
+              )
+
+
+          ),
         ),
-        body: Center(
-          child: Text('Welcome to our app!'),
-        ),
-      ),
-    );
+      );
+
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(),
+        floatingActionButton: Container(
+            height: 100.0,
+            width: 100.0,
+            child: FittedBox(
+              child: FloatingActionButton(
+                  child: Icon(Icons.warning),
+                  backgroundColor: Colors.red[500],
+                  onPressed: () {
+                    showAlertDialog(context);
+                  }),
+            )
+        )
+    );
+  }
+}
+
+void showAlertDialog(BuildContext context) {
+  // set up the buttons
+  Widget helpBtn = FlatButton(
+    child: Text("I need Help"),
+    onPressed:  () {
+      Navigator.pop(context);
+    },
+  );
+  Widget reportBtn = FlatButton(
+    child: Text("Report a Medical Emergency"),
+    onPressed:  () {
+      Navigator.pop(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Confirm your case to proceed"),
+    //content: Text("Would you like to continue learning how to use Flutter alerts?"),
+    actions: [
+      helpBtn,
+      reportBtn,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
